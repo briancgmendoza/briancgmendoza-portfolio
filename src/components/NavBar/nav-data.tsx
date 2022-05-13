9afbdd8033db9d626d8aa7d8f155d824ac022bd8
Brian Christopher Mendoza <78596685+briancgmendoza@users.noreply.github.com>
2022-05-13 12:55:50 +0800

    Adding Axios for fetching my profile in GitAPI. Creating GitProfile Component and minor fix in some components

---
+++ src/components/NavBar/nav-data.tsx
@@ -0,0 +1,14 @@
+export const navData = [
+  {
+    link: "#about",
+    name: "About",
+  },
+  {
+    link: "#projects",
+    name: "Projects",
+  },
+  {
+    link: "#messageMe",
+    name: "Message Me",
+  },
+];
