bb4b04ce3a23ab139c6767356f5a9b529eecb5ab
Brian Christopher Mendoza <78596685+briancgmendoza@users.noreply.github.com>
2022-05-14 20:03:55 +0800

    Adding Project Section with its data

--- custom.d.ts
+++ custom.d.ts
@@ -2,3 +2,13 @@ declare module "*.svg" {
   const content: any;
   export default content;
 }
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
